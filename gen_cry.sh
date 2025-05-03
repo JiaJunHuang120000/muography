3edb186ccf361a1552f6a4929e361ea258b46e7a
Jiajun Huang <jhuan328@ucr.edu>
2025-05-02 17:39:33 -0700

    Clean workspace

--- gen_cry.sh
+++
@@ -1,8 +0,0 @@
-cd cry_v1.7/test/
-./testMain ../../setup.file 9999 > output.txt
-
-cd ../..
-mkdir -p hepmc
-./cry_muon_converter cry_v1.7/test/output.txt hepmc/output.hepmc
-rm cry_v1.7/test/output.txt
-
