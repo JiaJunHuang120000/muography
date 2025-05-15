a907f4c8a6a3471734e80ca974bd1a838f2587b8
Jiajun Huang <jhuan328@ucr.edu>
2025-05-14 17:05:47 -0700

    EIC compile and sim running

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ echo "Compiled CRY Library ############################"
 bash bash/gen_cry.sh
 echo "Generated CRY Cosmic Muons ###########################"
 
-#curl -L https://github.com/eic/eic-shell/raw/main/install.sh | bash
-#./eic-shell
-#source setup_env.sh
-#bash build.sh
-#ddsim --compactFile xml/metal.xml --outputFile data/test.root --inputFile hepmc/output.hepmc --numberOfEvents 5
+curl -L https://github.com/eic/eic-shell/raw/main/install.sh | bash
+./eic-shell
+source setup_env.sh
+bash build.sh
+ddsim --compactFile xml/metal.xml --outputFile data/test.root --inputFile hepmc/output.hepmc --numberOfEvents 5
