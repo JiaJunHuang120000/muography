7948536b4456baca89ae9afcd1a269fd3ae6652e
Jiajun Huang <jhuan328@ucr.edu>
2025-10-07 21:11:30 -0700

    change directory path to local path

--- setup_env.sh
+++ setup_env.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 source /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-ubuntu2204-gcc11-opt/setup.sh
-source /home/jiajun/DD4hep/bin/thisdd4hep.sh
+source $PWD/DD4hep/bin/thisdd4hep.sh
 # --- Detector XML path ---
-export DETECTOR_PATH=~/muography_backup
+export DETECTOR_PATH=$PWD
 
 # --- Plugin library path ---
-export LD_LIBRARY_PATH=~/muography_backup/build:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PWD/build:$LD_LIBRARY_PATH
 
 # --- DD4hep binaries and other dependencies ---
-export PATH=~/DD4hep/bin:$PATH
-export LD_LIBRARY_PATH=~/DD4hep/lib:$LD_LIBRARY_PATH
-
-# Optional: source ROOT if not already sourced
-#source /cvmfs/sft.cern.ch/lcg/releases/ROOT/6.28.04-*/bin/thisroot.sh
-
+export PATH=$PWD/DD4hep/bin:$PATH
+export LD_LIBRARY_PATH=$PWD/DD4hep/lib:$LD_LIBRARY_PATH
