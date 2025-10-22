93540b3e49dc05d12c791348e961910ae32eacbf
Jiajun Huang <jhuan328@ucr.edu>
2025-10-22 09:09:51 -0700

    Working version for ddsim with new cvmfs version compatible with edm4hep output and hepmc3

--- bash/dd4hep_compile.sh
+++ bash/dd4hep_compile.sh
@@ -1,11 +1,12 @@
-source /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-ubuntu2204-gcc11-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_106/x86_64-ubuntu2204-gcc11-opt/setup.sh
 
-git clone https://github.com/AIDASoft/DD4hep.git
+git clone https://github.com/AIDASoft/DD4hep.git 
+
+cd DD4hep
 
 mkdir build && cd build
 
-cmake -DDD4HEP_USE_GEANT4=ON -DBoost_NO_BOOST_CMAKE=ON -DDD4HEP_USE_HEPMC=ON -DBUILD_TESTING=ON -DROOT_DIR=$ROOTSYS -D CMAKE_BUILD_TYPE=Release ..
+cmake -DDD4HEP_USE_GEANT4=ON -DBoost_NO_BOOST_CMAKE=ON -DDD4HEP_USE_HEPMC3=ON -DBUILD_TESTING=ON -DROOT_DIR=$ROOTSYS -DDD4HEP_USE_EDM4HEP=ON -D CMAKE_BUILD_TYPE=Release ..
 
 make install -j32
-cd ..
-
+cd ../..
--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-ubuntu2204-gcc11-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_106/x86_64-ubuntu2204-gcc11-opt/setup.sh
 source $PWD/DD4hep/bin/thisdd4hep.sh
 # --- Detector XML path ---
 export DETECTOR_PATH=$PWD
