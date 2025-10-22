aa9a81fec1f7b4eee913738acc0dae8bedf935ff
Jiajun Huang <jhuan328@ucr.edu>
2025-10-22 12:29:44 -0700

    rm .ipy

--- bash/.ipynb_checkpoints/build-checkpoint.sh
+++
@@ -1,2 +0,0 @@
-rm -rf build
-mkdir build; cd build; cmake .. -DCMAKE_INSTALL_PREFIX=$EIC_SHELL_PREFIX; make; make install;
--- bash/.ipynb_checkpoints/gen_cry-checkpoint.sh
+++
@@ -1,8 +0,0 @@
-cd cry_v1.7/test/
-./testMain ../../setup.file 100 > output.txt
-
-cd ../..
-mkdir -p hepmc
-./back_project_single cry_v1.7/test/output.txt hepmc/output.hepmc 100 0 0 0 0.1 10
-rm cry_v1.7/test/output.txt
-
