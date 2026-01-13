036e0863710424c45418cdc2f1e89c88c69b8f91
Jiajun Huang <jhuan328@ucr.edu>
2026-01-13 15:09:06 -0800

    Clean operation with clear variables used for simulation.

--- bash/iteration.sh
+++ bash/iteration.sh
@@ -1,29 +1,33 @@
+#!/bin/bash
+
+# Start measuring time for DDsim
 start=$(date +%s)
-numEvent=${number_of_events}
-configs=${world_configurations}
-x=${detector_pos_x}
-y=${detector_pos_y}
-z=${detector_pos_z}
-hepmc_path=${input_hepmc_path}
 
-for j in $(seq 0 ${#world_configurations}); do
-    for i in $(seq 0 ${number_of_detector}); do
-    
-        export DET_X=${x[i]}
-        export DET_Y=${y[i]}
-        export DET_Z=${z[i]}
-        export CONFIG=${configs[j]}
+numEvent=("${number_of_events[@]}")
+configs=("${world_configurations[@]}")
+hepmc_path="${hepmc_file_path}"
 
+# Loop over world configurations and detectors for DDsim
+for j in $(seq 0 $(( ${#configs[@]} - 1 ))); do
+    for i in $(seq 0 $((number_of_detector - 1))); do
         
-        ddsim \
-            --compactFile ${input_detector_file} \
-            --inputFiles   "${hepmc_file_path}/${detector_name}_${i}.hepmc" \
-            --outputFile   "${output_file_path}/${detector_name}_${configs[j]}_${i}.edm4hep.root" \
-            --numberOfEvents ${numEvent[i]} \
-    	    --steeringFile ${DETECTOR_PATH}/steering.py
-    done
+        # Export variables for DDsim
+        export DET_X="${x[$i]}"
+        export DET_Y="${y[$i]}"
+        export DET_Z="${z[$i]}"
+        export CONFIG="${configs[$j]}"
 
+        # Run DDsim with the current configuration
+        ddsim \
+            --compactFile "${input_detector_file}" \
+            --inputFiles "${hepmc_file_path}/${detector_name}_${i}.hepmc" \
+            --outputFile "${output_file_path}/${detector_name}_${configs[$j]}_${i}.edm4hep.root" \
+            --numberOfEvents "${numEvent[$i]}" \
+            --steeringFile "${DETECTOR_PATH}/steering.py"
+    done
 done
+
+# End time for DDsim simulation
 end=$(date +%s)
 runtime=$((end-start))
-echo "Total runtime: ${runtime} seconds"
+echo "Total runtime for DDsim simulation: ${runtime} seconds"
--- bash/multi.sh
+++ bash/multi.sh
@@ -1,21 +1,26 @@
+#!/bin/bash
+
+# Start measuring time
 start=$(date +%s)
 
-numEvent=${CRY_num_of_events}
-x=${detector_pos_x}
-y=${detector_pos_y}
-z=${detector_pos_z}
-offset=${detector_total_length}/2
-cut=${energy_cutoff}
+# Assign arrays from environment variables
+numEvent=("${CRY_num_of_events[@]}")
+x=("${detector_pos_x[@]}")
+y=("${detector_pos_y[@]}")
+z=("${detector_pos_z[@]}")
+offset=$(echo "${detector_total_length}/2" | bc -l)  # Ensure offset is a float
+cut=("${energy_cutoff[@]}")
 
-
-for i in $(seq 0 ${number_of_detector}); do
-	#output_file="${output_file_path}/${detector_name}_${i}.txt"
-	hepmc_file="${hepmc_file_path}/${detector_name}_${i}.hepmc"
-	#./testMain setup.file ${numEvent[i]} > ${output_file}
-	./remote ${input_cry} ${hepmc_file} ${generation_height} ${x[i]} ${y[i]} ${z[i]} ${offset} ${cut[i]} ${numEvent[i]}
-	#rm ${output_file}
-
+# Loop over detectors to generate HEPMC files
+for i in $(seq 0 $((number_of_detector - 1))); do
+    hepmc_file="${hepmc_file_path}/${detector_name}_${i}.hepmc"
+    echo "Running HEPMC generation for detector ${i}..."
+    
+    ./remote "${input_cry_file}" "${hepmc_file}" "${generation_height}" "${x[$i]}" "${y[$i]}" "${z[$i]}" "${offset}" "${cut[$i]}" "${numEvent[$i]}"
+    
 done
+
+# End time for HEPMC generation
 end=$(date +%s)
 runtime=$((end-start))
-echo "Total runtime: ${runtime} seconds"
+echo "Total runtime for HEPMC generation: ${runtime} seconds"
